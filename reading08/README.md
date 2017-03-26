5c8afc41774294d7d5d19aea9b38971b1a9de09e
Brianna Dawn Hoelting <bhoeltin@student00.cse.nd.edu>
2017-03-26 11:17:34 -0400

    reading08:complete

--- reading08/README.md
+++ reading08/README.md
@@ -1,3 +1,14 @@
 Reading 08
 ==========
 
+1. 1) 4 bytes
+   2) 20 bytes
+   3) 5 bytes
+   4) 16 bytes
+   5) 8 bytes
+   6) 16 bytes
+   7) 160 bytes
+   8) 80 bytes
+
+2. The memory error in Task 3 was that there was not enough memory allocated for the array of random numbers. This is because the malloc sis not used sizeof to allocate the memory. I fixed this by implementing the sizeof function. The memory error in Task 4 related to the memory not being freed properly. I fixed this by adding a free memory option before the if statement returned true. In this way if a duplicate was found, then the memory would be freed. Previously, the function would have just exited without the memory being freed becuas ethe free memory thing was outside teh for loop.
+   
---
+++ reading08/duplicates.c
@@ -0,0 +1,53 @@
+/* duplicates.c */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int NITEMS = 1<<10;
+const int TRIALS = 100;
+
+bool contains(int *a, int n, int k) {
+    /* TODO: Search through array to see if there is an element that matches key */
+
+  for (int i = 0; i < n; i++){
+    
+    if (*a == k){
+      return true;
+	}
+    a++;
+  }
+
+    return false;
+}
+
+bool duplicates(int n) {
+  int *randoms = malloc(sizeof(n)*n) ;
+
+    for (int i = 0; i < n; i++)
+    	randoms[i] = rand() % 1000;
+
+    for (int i = 0; i < n; i++) {
+        /* TODO: Use contains to search array for duplicate */ 
+      if (contains(randoms, n, randoms[i])) {
+	free(randoms);
+    	return true;
+      }
+    }
+
+    free(randoms);
+    return false;
+}
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
+    for (int i = 0; i < TRIALS; i++)
+    	if (duplicates(NITEMS))
+	    puts("Duplicates detected!");
+
+    return 0;
+}
+
+/* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
